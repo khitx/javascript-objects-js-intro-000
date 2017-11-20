11cabc0bacd628dff3bebb4e5f8fbf88ac1add15
Kathy Hudson <khitx@icloud.com>
2017-11-20 23:58:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ var playlist = {demons:'imagine dragons'}
 function updatePlaylist(obj,key,value) {
   return Object.assign(obj,{[key]:value})
 }
+
+function removeFromPlaylist(obj,key) {
+  delete obj[key]
+  return obj
+}
