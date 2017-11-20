09e69456cc5a8294f9887bca5602532f5d47d41c
Kathy Hudson <khitx@icloud.com>
2017-11-20 23:23:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = {demons:'imagine dragons'}
 
-function updatePlaylist(playlist,artistName,songTitle) {
-  return Object.assign(playlist,{[artistName]:songTitle})
+//function updatePlaylist(playlist,artistName,songTitle) {
+//  return Object.assign(playlist,{[artistName]:songTitle})
+//}
+
+function updatePlaylist(obj,key,value) {
+  return Object.assign(obj,{[key]:value})
 }
